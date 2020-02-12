0b53c1c10bd2d2a20caf4ba0984757a75452988e
Carlos Perez-Lopez <carlospzlz@gmail.com>
2020-02-12 22:27:10 +0000

    Modified ycm_extra_conf to work with OpenCV

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -9,7 +9,10 @@ def FlagsForFile(filename, **kwargs):
             '-isystem', '/usr/include/c++/9.2.0',
             '-isystem', '/usr/local/include',
             '-isystem', '/usr/include',
+            '-isystem', '/usr/include/opencv4/opencv2/opencv.hpp',
             '-I', 'include',
             '-I', '/home/infcpl00/clones/USD/install/include',
+            '-I', '/home/infcpl00/foundry/Katana3.5v1.010022b/plugin_apis/include',
+            '-I', '/home/infcpl00/dev/hydra-selection/include',
             ]
     }
